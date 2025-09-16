c9266f1a7b19311bbbdde42b39701a90121b4759
Marx Gabas <bgabasmarxnathaniel@gmail.com>
2025-09-16 19:54:45 +0800

    Added Gabas page

---
+++ app/Gabas/page.tsx
@@ -0,0 +1,9 @@
+import Info from './info';
+export default function Page() {
+    return (
+        <div>
+        <h1 className="text-3x1 font-bold">Marx Gabas</h1>
+        <Info/>
+        </div>
+    )
+}
